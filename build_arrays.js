5372cf383999909f1e3f01d59587030852ccf296
romanarellano <romanarellano@yahoo.com>
2015-01-20 18:24:04 -1000

    arrays with functions

--- build_arrays.js
+++ build_arrays.js
@@ -8,7 +8,7 @@
 // Create an array of the colors of the rainbow.
 
 
-// Create an array of gems.
+// Create an array of gems
 
 
 // Create an array of military ranks.
@@ -62,20 +62,30 @@
 // Create an array of Objects that represent Famous People.
 // Each Object should have a Name, Age, and their Profession as properties
 
-
+var famous_people = [{
+  Name:"Dave Chappelle",Age:43,Profession:"Comedian"},{
+  Name:"Michael Jordan",Age:53,Profession:"Golfer"},{
+  Name:"Rihanna",Age:25,Profession:"Singer"}
+    ];
 // Create an array of songs you'd like Jon to add to the playlist of music 
 // to listen to in class.  Each song should be an object with at least 
 // a title and an artist.
-
+var songs = [{artist:"Farruko",song:"6am"},{artist:"J Balvin",song:"Ay vamos"},{artist:"Enrique Iglesias",song:"bailando"}];
 
 // Create an array of functions. Define any functions you want, they can 
 // be anything! But make sure the syntax is correct :D
-
+var defunct = [function(){ return a*b; }, 
+function(){ console.log("lose yourself");
+}];
 
 // Create an array of Sublime shortcuts.  Each item should have a keystroke 
 // combination and a textual description of what the key combination does. 
 // You can find Sublime Text Shortcuts by traversing the Sublime Text's GUI
 // e.g. File Menu options.
 
+var short_cuts = new Array(2);
 
+short_cuts["command O"] = "Opens File";
+short_cuts["command C"] = "Copy File";
+short_cuts["command R"] = "Go to";
 
