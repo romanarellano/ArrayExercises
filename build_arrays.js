53645b67a11e629bcf844310646198095a758f5a
romanarellano <romanarellano@yahoo.com>
2015-01-20 17:11:55 -1000

    arrays created

--- build_arrays.js
+++ build_arrays.js
@@ -3,54 +3,54 @@
  **/
 
 // Create an empty array.
-
+var arr = [];
 
 // Create an array of the colors of the rainbow.
 
-
+var rainbow_colors=["blue","green","yellow","red"];
 // Create an array of gems.
 
-
+var gems=["ruby","emerald","quartz","kyanite"];
 // Create an array of military ranks.
 
-
+var military_ranks=["first lieutenant","brigadier general","major general"];
 // Create an array of road signs.
 
-
+var road_signs=["deer crossing","closed to motorcycles","closed to trucks"];
 // Create an array containing components of a tree.
 
-
+var tree_components=["trunk","leaves","branches"];
 // Create an array of prime numbers less than 20.
 
-
+var primes = [2,3,5,7,11,13,17,19];
 // Create an array of words programmers typically say while programming.
 
-
+var program_words=["shit","fuck","array"];
 // Create an array of elements of the periodic table.
 
-
+var periodic_elements = ["Na","Au","Ca","Ag"];
 // Create an array representing the coins currently in your pocket.
 
-
+var coins = ["2 pennies","3 dimes"];
 // Create an array of items you bring to class daily.
 
-
+var items = ["toothbrush","underwear","beer"];
 // Create an array of web developer role titles.
 
-
+var dev_titles = ["front end","back end","database"];
 // Create an array of good luck charms.
 
-
+var good_luck_charms = ["horse shoe","lentils","goldfish"];
 // Create an array of mythical animals.
 
-
+var animals = ["unicorn","bigfoot","lockness"];
 // Create an array of drinks.
 
-
+var drinks = ["pisco","powerade","vodka"];
 // Create an array of emoticons.
 
-
+var emoticons = [null,"smiley face,","i dont know"];
 // Create an array with the letters of the word 'array'.
 
-
+var letters = ["a","r","r","a","y"];
 
